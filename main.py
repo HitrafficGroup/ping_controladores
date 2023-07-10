06cf9e303ca5e2bf92ce6aec1a4dc58d522ae2e9
david <david.diaz190799@gmail.com>
2023-07-10 12:18:45 -0500

    se crea el repositorio

---
+++ main.py
@@ -0,0 +1,41 @@
+import platform, subprocess
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+# Use a service account.
+cred = credentials.Certificate('credentials.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+def ping(host_or_ip, packets=1, timeout=1000):
+    #The ping command is the same for Windows and Linux, except for the "number of packets" flag.
+    if platform.system().lower() == 'windows':
+        command = ['ping', '-n', str(packets), '-w', str(timeout), host_or_ip]
+        result = subprocess.run(command, stdin=subprocess.DEVNULL, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, creationflags=0x08000000)
+        return result.returncode == 0 and b'TTL=' in result.stdout
+    else:
+        command = ['ping', '-c', str(packets), '-w', str(timeout), host_or_ip]
+        # run parameters: discard output and error messages
+        result = subprocess.run(command, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return result.returncode == 0
+
+
+def updateStateController(id,estado):
+    controller_ref = db.collection("historial_controladores").document(id)   
+    controller_ref.update({"online": estado})
+
+users_ref = db.collection("historial_controladores")
+docs = users_ref.stream()
+controllers_ip = []
+for doc in docs:
+    controllers_ip.append(doc.to_dict())
+
+for controller in controllers_ip:
+    if ping(controller["ip"]):
+        updateStateController(controller["id"],True)
+        print("tenemos ping del controlador: ",controller["ip"])
+    else:
+        updateStateController(controller["id"],False)
+        print("no tenemos ping del controlador: ",controller["ip"])
+
+
+
